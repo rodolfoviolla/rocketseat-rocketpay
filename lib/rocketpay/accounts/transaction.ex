56ae8b8607127780e3999ef0ca089d048e235b95
Rodolfo Della Violla <rodolfo.violla@eduzz.com>
2021-02-27 20:16:47 -0300

    Aula 4 na metade

---
+++ lib/rocketpay/accounts/transaction.ex
@@ -0,0 +1,24 @@
+defmodule Rocketpay.Account.Transaction do
+  alias Ecto.Multi
+
+  alias Rocketpay.{Account.Operation, Repo}
+
+  def call(%{"from" => from_id, "to" => to_id, "value" => value}) do
+    withdraw_params = build_params(from_id, value)
+    deposit_params = build_params(to_id, value)
+
+    Multi.new()
+    |> Multi.merge(fn _changes -> Operation.call(withdraw_params, :withdraw) end)
+    |> Multi.merge(fn _changes -> Operation.call(deposit_params, :deposit) end)
+    |> run_transaction()
+  end
+
+  defp build_params(id, value), do: %{"id" => id, "value" => value}
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{withdraw: from_account, deposit: to_account}} -> {:ok, %{from_account: from_account, to_account: to_account}}
+    end
+  end
+end
--- lib/rocketpay/accounts/withdraw.ex
+++ lib/rocketpay/accounts/withdraw.ex
@@ -1,16 +1,16 @@
-defmodule Rocketpay.Account.Deposit do
+defmodule Rocketpay.Account.Withdraw do
   alias Rocketpay.{Account.Operation, Repo}
 
   def call(params) do
     params
-    |> Operation.call(:deposit)
+    |> Operation.call(:withdraw)
     |> run_transaction()
   end
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{account_deposit: account}} -> {:ok, account}
+      {:ok, %{account_withdraw: account}} -> {:ok, account}
     end
   end
 end
